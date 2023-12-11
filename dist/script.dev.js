5de34400dd5c1c2b96f86425cf593d151f027d6e
alawayout <118168197+alawayout@users.noreply.github.com>
2023-12-11 12:45:06 +0300

    -

---
+++ dist/script.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function toggleDropdown() {
+  var dropdown = document.getElementById("myDropdown");
+
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+  } else {
+    dropdown.style.display = "block";
+  }
+}
